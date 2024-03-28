7935173cf8e5d4d8e70b8070cb0d69380c35726f
Andrew Wu <a-wu.1@onu.edu>
2024-03-28 10:04:14 -0400

    Completed P8-2.

--- Project18/FileName.cpp
+++ Project18/FileName.cpp
@@ -1,1 +1,33 @@
-a
+#include <iostream>
+#include <string>
+using namespace std;
+int countWords(string str)
+{
+	int word = 1;
+	int string = str.size();
+	for (int i = 0; i < string; i++)
+		if (str[i] == ' ')
+		{
+			word++;
+		}
+	return word;
+}
+int main()
+{
+	bool input = true;
+	while (input)
+	{
+		string str;
+		cout << "Enter string or Q to quit: ";
+		getline(cin, str);
+		int result = countWords(str);
+		if (str == "Q")
+		{
+			input = false;
+		}
+		else
+		{
+			cout << "Word count: " << result << endl;
+		}
+	}return 0;
+}
